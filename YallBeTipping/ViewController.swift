9463872f1dbed4fc0b47fc305b328ee76e1ab4a7
robertpapp <papprobert89@gmail.com>
2025-02-27 10:01:46 +0100

    Initial Commit

---
+++ YallBeTipping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YallBeTipping
+//
+//  Created by Robert Papp on 2025. 02. 27..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YallBeTippingTests/YallBeTippingTests.swift
@@ -0,0 +1,17 @@
+//
+//  YallBeTippingTests.swift
+//  YallBeTippingTests
+//
+//  Created by Robert Papp on 2025. 02. 27..
+//
+
+import Testing
+@testable import YallBeTipping
+
+struct YallBeTippingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
